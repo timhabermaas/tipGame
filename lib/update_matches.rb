6239bb4129dea31a4f8c5a2b1c08ffd84e16b108
Tim Habermaas <tim.habermaas@gmail.com>
2012-05-30 23:51:27 +0200

    add half time scores

--- lib/update_matches.rb
+++ lib/update_matches.rb
@@ -2,16 +2,29 @@ class UpdateMatches
   def self.perform
     OpenLigaMatch.all.each do |m|
       match = Match.find_by_match_id m[:match_id]
-      if match.nil?
-        Match.create!(:match_id => m[:match_id],
-                      :team_1_name => m[:name_team1],
-                      :team_2_name => m[:name_team2],
-                      :finished => m[:match_is_finished],
-                      :goals_team_1 => m[:points_team1],
-                      :goals_team_2 => m[:points_team2],
-                      :starts_at => m[:match_date_time_utc])
+      results = m[:match_results] # TODO move to OpenLigaMatch class
+      results = results[:match_result] if results.kind_of? Hash
+      if results
+        half_results = results.find { |r| r[:result_order_id].to_i == 2}
       else
-        match.update_attributes(:goals_team_1 => 4, :goals_team_2 => 3)
+        half_results = {}
+      end
+      mapping = {
+        :match_id => m[:match_id],
+        :team_1_name => m[:name_team1],
+        :team_2_name => m[:name_team2],
+        :finished => m[:match_is_finished],
+        :goals_team_1 => m[:points_team1],
+        :goals_team_2 => m[:points_team2],
+        :goals_first_half_team_1 => half_results[:points_team1],
+        :goals_first_half_team_2 => half_results[:points_team2],
+        :starts_at => m[:match_date_time_utc],
+        :round => m[:group_name]
+      }
+      if match.nil?
+        Match.create!(mapping)
+      else
+        match.update_attributes(mapping)
       end
     end
   end
--- spec/update_matches_spec.rb
+++ spec/update_matches_spec.rb
@@ -14,7 +14,10 @@ describe UpdateMatches do
       :points_team2 => 3,
       :match_date_time_utc => "2010-08-21T18:43:28.94",
       :name_team1 => "Deutschland",
-      :name_team2 => "Frankreich"
+      :name_team2 => "Frankreich",
+      :group_name => "Achtelfinale",
+      :match_results => [{:result_order_id => 1, :points_team1 => 4, :points_team2 => 3},
+                         {:result_order_id => 2, :points_team1 => 2, :points_team2 => 1}]
     }
   end
 
@@ -22,7 +25,7 @@ describe UpdateMatches do
     it "adds match to database" do
       OpenLigaMatch.should_receive(:all).and_return [response]
       match_class.should_receive(:find_by_match_id).with(4).and_return nil
-      match_class.should_receive(:create!).with(hash_including(:match_id => 4, :team_1_name => "Deutschland"))
+      match_class.should_receive(:create!).with(hash_including(:match_id => 4, :team_1_name => "Deutschland", :round => "Achtelfinale", :goals_first_half_team_2 => 1))
       UpdateMatches.perform
     end
   end
@@ -38,10 +41,21 @@ describe UpdateMatches do
     end
 
     it "updates the existing match" do
-      match.should_receive(:update_attributes).with(hash_including(:goals_team_1 => 4, :goals_team_2 => 3))
+      match.should_receive(:update_attributes).with(hash_including(:goals_team_1 => 4, :goals_team_2 => 3, :round => "Achtelfinale", :goals_first_half_team_1 => 2))
       OpenLigaMatch.should_receive(:all).and_return [response]
       match_class.should_receive(:find_by_match_id).with(4).and_return match
       UpdateMatches.perform
+    end
+
+    context "isn't finished and has no match results" do
+      before { response.delete(:match_results) }
+
+      it "doesn't crash" do
+        match.should_receive(:update_attributes).with(hash_including(:goals_team_1 => 4, :goals_team_2 => 3, :round => "Achtelfinale", :goals_first_half_team_1 => nil))
+        OpenLigaMatch.should_receive(:all).and_return [response]
+        match_class.should_receive(:find_by_match_id).with(4).and_return match
+        UpdateMatches.perform
+      end
     end
   end
 end
