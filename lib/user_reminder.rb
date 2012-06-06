d53c7de84308f4d8d4cc55961597d8f6f1721c3f
Tim Habermaas <tim.habermaas@gmail.com>
2012-06-06 04:02:39 +0200

    add forgotten #deliver call *argh*

--- lib/user_reminder.rb
+++ lib/user_reminder.rb
@@ -1,7 +1,7 @@
 class UserReminder
   def self.remind(users, user_mailer = UserMailer)
     users.each do |user|
-      user_mailer.remind_about_matches(user, user.forgotten_matches) if user.remindable?
+      user_mailer.remind_about_matches(user, user.forgotten_matches).deliver if user.remindable?
     end
   end
 end
--- spec/lib/user_reminder_spec.rb
+++ spec/lib/user_reminder_spec.rb
@@ -1,6 +1,7 @@
 require "user_reminder"
 
 describe UserReminder do
+  let(:mail) { stub(:mail) }
   let(:user_mailer) { stub(:user_mailer) }
 
   context "#remind" do
@@ -9,15 +10,17 @@ describe UserReminder do
     let(:user2) { stub(:user2, :forgotten_matches => [], :remindable? => false) }
 
     it "checks all users and reminds them by email if they forgot to bet on a match" do
-      user_mailer.should_receive(:remind_about_matches).with(user1, matches)
+      user_mailer.should_receive(:remind_about_matches).with(user1, matches) { mail }
+      mail.should_receive(:deliver)
+
       UserReminder.remind([user1, user2], user_mailer)
     end
 
     it "doesn't send an email if the user was reminded less than 24 hours ago" do
       user1.stub(:remindable? => false)
+      user_mailer.should_not_receive(:remind_about_matches)
 
       UserReminder.remind([user1, user2], user_mailer)
-      user_mailer.should_not_receive(:remind_about_matches)
     end
   end
 end
